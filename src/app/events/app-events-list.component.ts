ba7414711382d06bce9e17385eca2175c75991f0
watchain <watchain@yahoo.com>
2019-08-14 09:37:11 -0700

    ng-fundamental start

---
+++ src/app/events/app-events-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  template: `
+    <div>
+      <h1>Uncoming Angular Events</h1>
+    </div>
+  `
+})
+
+
+export class EventsListComponent {
+  title = 'App';
+}
+
